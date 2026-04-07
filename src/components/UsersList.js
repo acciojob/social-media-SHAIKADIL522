81faa0ad1ec75896524a70b99d5bbe8a0cc1d2b6
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-07 11:57:27 +0530

    fix:updated the userlist file

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -3,7 +3,8 @@ import React from "react";
 const UsersList = ({ users }) => {
   return (
     <ul>
-      {users.map(u => (
+      {/* ✅ Only first 3 users */}
+      {users.slice(0, 3).map(u => (
         <li key={u.id}>
           <a href={`/users/${u.id}`}>{u.name}</a>
         </li>
